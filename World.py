06a3dd3e0aefe4f9e677cbce317a69735e17d625
Erisiordian <hat.luka10@gmail.com>
2026-03-03 18:51:58 +0100

    Add files via upload

--- World.py
+++ World.py
@@ -1,1 +1,9 @@
 print("Hello World")
+
+def addnum():
+    fnum = int(input("Enter first number: "))
+    snum = int(input("Enter second number: "))
+    sum = fnum + snum
+    print("The sum of ",fnum,"and ",snum,"is ",sum)
+
+addnum()
